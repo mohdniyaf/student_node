1bb7d0037cee2fe4066fe8ba74ab3ab288dc50fd
mohammedniyaf <mohammedniyaf77@gmail.com>
2024-05-25 20:27:38 -0700

    update (put) and delete added

--- router/student_router.js
+++ router/student_router.js
@@ -30,4 +30,45 @@ router.post('/',async(req,res)=>{
     }
  })
 
+ router.put('/:id', async (req, res) => {
+    try {
+        const studentId = req.params.id; 
+        const studentUpdatedData = req.body;
+
+        const response = await Person.findByIdAndUpdate(studentId, studentUpdatedData, {
+            new: true,
+            runValidators: true,
+        })
+
+        if (!response) {
+            return res.status(404).json({ err: "person id not found" });
+        }
+        console.log("data updated");
+        res.status(200).json(response);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ err: "internal error" });
+    }
+});
+
+    router.delete('/:id',async (req,res)=>{
+        try {
+        const studentId=req.params.id;
+        const studentdeleteData=req.body;
+
+        const response=await person.findByIdAndDelete(studentId,studentdeleteData);
+
+        if (!response) {
+            return res.status(404).json({ err: "person id not found" });
+        }
+
+        console.log("deleted data");
+        res.status(200).json({message:"sucesufully deleted"});
+        } catch (error) {
+             console.log(error);
+        res.status(500).json({ err: "internal error" });
+        }
+    })
+
+
  module.exports=router;
